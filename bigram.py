15e5ec264bcd89958544cee26c8bd0dbc12a0fd3
astroid19 <85995121+astroid19@users.noreply.github.com>
2024-04-04 16:57:47 +0300

    partial implementation

--- bigram.py
+++ bigram.py
@@ -5,7 +5,7 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
-max_iters = 5000
+max_iters = 5001
 eval_interval = 500
 learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -82,7 +82,52 @@ class Head(nn.Module):
         v = self.value(x)   # (B, T, C)
         out = wei @ v  # (B, T, C)
         return out
+    
+class MultiHeadAttention(nn.Module):
+    ''' multiple heads of self-attention '''
 
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(num_heads * head_size, n_embd)
+    
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return torch.cat([h(x) for h in self.heads], dim=-1)   
+    
+class FeedFoward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd)
+        )
+
+    def forward(self, x):
+
+            return self.net(x)
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
+        
+    def forward(self, x):
+        x = x + self.sa(x)  # x + for residual connections 
+        x = x + self.ffwd(x)
+        return x
+        
+        
+        
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -91,7 +136,11 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd) 
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4)
+        )
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -101,8 +150,8 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx)  # (B, T, n_embd)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T, n_embd)
         x = tok_emb + pos_emb  # (B, T, n_embd)
-        x = self.sa_head(x)  # (B, T, n_embd) 
-        logits = self.lm_head(tok_emb)  # (B, T, vocab_size) 
+        x = self.blocks(x)  # (B, T, n_embd) 
+        logits = self.lm_head(x)  # (B, T, vocab_size) 
 
         if targets is None:
             loss = None
